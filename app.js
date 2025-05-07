11013504fa1f084b304ed06627115db2218f8042
chloeho97 <chloe-ho@hotmail.fr>
2025-05-08 00:27:03 +0200

    link front

--- app.js
+++ app.js
@@ -25,7 +25,7 @@ const app = express();
 const cors = require("cors");
 app.use(
   cors({
-    origin: "https://arcana-front-2.vercel.app",
+    origin: "https://arcana-front-two.vercel.app",
     credentials: true,
   })
 );
