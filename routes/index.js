71a2262b794716984bb01577e097049e101d970b
chloeho97 <chloe-ho@hotmail.fr>
2025-05-07 22:07:30 +0200

    edit title

--- routes/index.js
+++ routes/index.js
@@ -5,7 +5,7 @@ const User = require("../models/users");
 
 /* GET home page. */
 router.get("/", function (req, res, next) {
-  res.status(200).json({ message: "index", title: "Express" });
+  res.status(200).json({ message: "index", title: "back-end Arcana" });
 });
 
 module.exports = router;
